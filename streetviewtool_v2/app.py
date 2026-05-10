772d278aa56fb89821de09ac922de15c1aa39d2a
LYF <maxours2008@gmail.com>
2026-05-10 17:19:30 +0200

    Add files via upload

--- streetviewtool_v2/app.py
+++ streetviewtool_v2/app.py
@@ -79,7 +79,8 @@ def generate():
 
 '''Run the app'''
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
 
 
 
